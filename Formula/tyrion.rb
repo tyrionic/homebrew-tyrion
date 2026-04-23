7405e2d1c34bf924ef91ef51cc0c649d19ecd05f
Dennis Vink <dennis.vink@xebia.com>
2026-04-23 08:13:53 +0200

    Add release tag to formula

--- Formula/tyrion.rb
+++ Formula/tyrion.rb
@@ -3,27 +3,28 @@ class Tyrion < Formula
   homepage "https://github.com/tyrionic/tyrionic"
   version "0.1.3"
   license "MIT"
+  release_tag = "v0.1.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/tyrionic/tyrionic/releases/download/v0.1.3/tyrion_macos_aarch64"
+      url "https://github.com/tyrionic/tyrionic/releases/download/#{release_tag}/tyrion_macos_aarch64"
       sha256 "7f8f188686adea05634f204ab1bf931231c7687f06e67ddb81fbbc8c683c8550"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/tyrionic/tyrionic/releases/download/v0.1.3/tyrion_linux_x86"
+      url "https://github.com/tyrionic/tyrionic/releases/download/#{release_tag}/tyrion_linux_x86"
       sha256 "25eb03d1e8869dab80840755e02d1df154dfeeeba61692f5f10802f4d92f338c"
     end
     on_arm do
-      url "https://github.com/tyrionic/tyrionic/releases/download/v0.1.3/tyrion_linux_aarch64"
+      url "https://github.com/tyrionic/tyrionic/releases/download/#{release_tag}/tyrion_linux_aarch64"
       sha256 "62132152f20b84a7a07472cb97317b88223d69f1155c7897a27f1a5b3384f0a8"
     end
   end
 
   resource "tyrion_source" do
-    url "https://github.com/tyrionic/tyrionic/archive/refs/tags/v#{version}.tar.gz"
+    url "https://github.com/tyrionic/tyrionic/archive/refs/tags/#{release_tag}.tar.gz"
     sha256 "8660da529e0b512c20d20ea00841a9b65baf63d10e52d74bfafad1b277e5e253"
   end
 
